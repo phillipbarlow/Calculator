040922c7f5673d84803454015dfe67400669e3f1
phillipbarlow10@gmail.com <phillipbarlow10@gmail.com>
2025-07-06 18:33:19 +0100

    basic styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='calculator'>
+        <div className='display-screen'>
+
+        </div>
+        <div className='btnsContainer'>
+          <button className='clear'>C</button>
+          <button className='percent'>%</button>
+          <button className='divide'>/</button>
+          <button className='nine'>9</button>
+          <button className='eight'>8</button>
+          <button className='seven'>7</button>
+          <button className='six'>6</button>
+          <button className='five'>5</button>
+          <button className='four'>4</button>
+          <button className='three'>3</button>
+          <button className='two'>2</button>
+          <button className='one'>1</button>
+          <button className='zero'>0</button>
+          <button className='mult'>X</button>
+          <button className='minus'>-</button>
+          <button className='add'>+</button>
+          <button className='decimal'>.</button>
+          <button className='equal'>=</button>
+        </div>
+      </div>
     </div>
   );
 }
