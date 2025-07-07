277752eb0ba124cc22a6b311a6d2b31774f9da0a
phillipbarlow10@gmail.com <phillipbarlow10@gmail.com>
2025-07-07 18:57:04 +0100

    cleaning up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-
+import Button from './components/Button';
 function App() {
+
   return (
     <div className="App">
       <div className='calculator'>
@@ -8,24 +9,24 @@ function App() {
 
         </div>
         <div className='btnsContainer'>
-          <button className='clear'>C</button>
-          <button className='percent'>%</button>
-          <button className='divide'>/</button>
-          <button className='nine'>9</button>
-          <button className='eight'>8</button>
-          <button className='seven'>7</button>
-          <button className='six'>6</button>
-          <button className='five'>5</button>
-          <button className='four'>4</button>
-          <button className='three'>3</button>
-          <button className='two'>2</button>
-          <button className='one'>1</button>
-          <button className='zero'>0</button>
-          <button className='mult'>X</button>
-          <button className='minus'>-</button>
-          <button className='add'>+</button>
-          <button className='decimal'>.</button>
-          <button className='equal'>=</button>
+          <Button className='clear' symbol="c">C</Button>
+          <Button className='percent' symbol="%">%</Button>
+          <Button className='divide' symbol="/">/</Button>
+          <Button className='nine' symbol="9">9</Button>
+          <Button className='eight' symbol="8">8</Button>
+          <Button className='seven'symbol="7">7</Button>
+          <Button className='six' symbol="6">6</Button>
+          <Button className='five' symbol="5">5</Button>
+          <Button className='four' symbol="4">4</Button>
+          <Button className='three' symbol="3">3</Button>
+          <Button className='two' symbol="2">2</Button>
+          <Button className='one' symbol="1">1</Button>
+          <Button className='zero' symbol="0">0</Button>
+          <Button className='mult' symbol="*">X</Button>
+          <Button className='minus' symbol="-">-</Button>
+          <Button className='add' symbol="+">+</Button>
+          <Button className='decimal' symbol=".">.</Button>
+          <Button className='equal' symbol="=">=</Button>
         </div>
       </div>
     </div>
